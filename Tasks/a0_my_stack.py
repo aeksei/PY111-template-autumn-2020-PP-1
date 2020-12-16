b09a5e9438946bda1512027d4c05f1aee257a54a
aeksei <aeksei94@gmail.com>
2020-12-16 18:50:24 +0300

    реализовали функцию очистки

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -44,6 +44,7 @@ def clear() -> None:
 
     :return: None
     """
+    my_stack.clear()
     return None
 
 
@@ -53,4 +54,9 @@ if __name__ == '__main__':
     print(my_stack)
 
     push(2)
+    print(my_stack)
+
+    clear()
+    print(my_stack)
+    push(3)
     print(my_stack)
