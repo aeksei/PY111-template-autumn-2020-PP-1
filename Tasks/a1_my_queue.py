6ef8a43f3d4487b1c15fd9a466e17531483546ea
aeksei <aeksei94@gmail.com>
2020-12-16 21:02:19 +0300

    fix index check

--- Tasks/a1_my_queue.py
+++ Tasks/a1_my_queue.py
@@ -33,7 +33,7 @@ def peek(ind: int = 0) -> Any:
     :param ind: index of element (count from the beginning)
     :return: peeked element
     """
-    return None if ind > len(my_queue) else my_queue[-1 - ind]
+    return None if ind >= len(my_queue) else my_queue[-1 - ind]
 
 
 def clear() -> None:
